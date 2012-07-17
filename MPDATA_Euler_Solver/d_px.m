2f4d34b4b41909dbd583c5164d428e5fc99f22b7
slsd123 <Sean@Seans-MacBook-Pro.local>
2012-07-17 16:27:25 -0600

    MPDATA_Solver
    Here is my full MPDATA Solver, which seems to work

---
+++ MPDATA_Euler_Solver/d_px.m
@@ -0,0 +1,10 @@
+function f = d_px(j,i,R)
+order = 2;
+if order == 1
+% First Order in the x direction
+    f = (R(j,i+1)-R(j,i  ))./(R(j,i+1)+R(j,i  )+eps);
+elseif order == 2
+% Second order in the x direction
+    f = (-R(j,i+2)+4*R(j,i+1)-3*R(j,i))./(R(j,i+2)+4*R(j,i+1)+3*R(j,i)+eps)*2;
+end
+end
