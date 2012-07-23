96c2cb0fbd131e8968c0524250131499cf746631
Will <wmlm@will-laptop.(none)>
2012-07-23 09:54:49 -0600

    Updated results in poster.

--- Fin_Diff_Advection/DATA/Plotting.m
+++ Fin_Diff_Advection/DATA/Plotting.m
@@ -6,7 +6,8 @@ for i = [1,5,10]
     phi_analyt = S.phi_analyt;
     clear S;
     h = figure(i); hold on, plot(0:length(phi)-1,phi(:,34),'o'), plot(0:length(phi)-1,phi_analyt(:,34),'r')
-    legend([type,'-SUPG'],'Analytical'), title([type,'-SUPG ','at t = ',num2str(i)]), axis([0,length(phi)-1,-.5,1.5]);
+    %legend([type,'-SUPG'],'Analytical'), title([type,'-SUPG ','at t = ',num2str(i)]), 
+    axis([0,length(phi)-1,-.5,1.5]); set(gca,'FontSize',14);
     hold off
     saveas(h,[type,'_',num2str(i),'.png']);
 end
@@ -18,7 +19,8 @@ for i = [1,5,10]
     phi_analyt = S.phi_analyt;
     clear S;
     h = figure(i); hold on, plot(0:length(phi)-1,phi(:,34),'o'), plot(0:length(phi)-1,phi_analyt(:,34),'r')
-    legend('Lax-Wendroff-noTVD','Analytical'), title(['Lax-Wendroff-noTVD ','at t = ',num2str(i)]), axis([0,length(phi)-1,-.5,1.5]);
+    %legend('Lax-Wendroff-noTVD','Analytical'), title(['Lax-Wendroff-noTVD ','at t = ',num2str(i)]), 
+    axis([0,length(phi)-1,-.5,1.5]); set(gca,'FontSize',14);
     hold off
     saveas(h,[type,'_',num2str(i),'.png']);
 end
@@ -30,7 +32,8 @@ for i = [1,5,10]
     phi_analyt = S.phi_analyt;
     clear S;
     h = figure(i); hold on, plot(0:length(phi)-1,phi(:,34),'o'), plot(0:length(phi)-1,phi_analyt(:,34),'r')
-    legend('Lax-Wendroff-superbee','Analytical'), title(['Lax-Wendroff-superbee ','at t = ',num2str(i)]), axis([0,length(phi)-1,-.5,1.5]);
+    %legend('Lax-Wendroff-superbee','Analytical'), title(['Lax-Wendroff-superbee ','at t = ',num2str(i)]), 
+    axis([0,length(phi)-1,-.5,1.5]); set(gca,'FontSize',14);
     hold off
     saveas(h,[type,'_',num2str(i),'.png']);
 end
@@ -42,7 +45,8 @@ for i = [1,5,10]
     phi_analyt = S.phi_analyt;
     clear S;
     h = figure(i); hold on, plot(0:length(phi)-1,phi(:,34),'o'), plot(0:length(phi)-1,phi_analyt(:,34),'r')
-    legend(type,'Analytical'), title([type,' at t = ',num2str(i)]), axis([0,length(phi)-1,-.5,1.5]);
+    %legend(type,'Analytical'), title([type,' at t = ',num2str(i)]), 
+    axis([0,length(phi)-1,-.5,1.5]); set(gca,'FontSize',14);
     hold off
     saveas(h,[type,'_',num2str(i),'.png']);
 end
@@ -54,7 +58,8 @@ for i = [1,5,10]
     phi_analyt = S.phi_analyt;
     clear S;
     h = figure(i); hold on, plot(0:length(phi)-1,phi(:,34),'o'), plot(0:length(phi)-1,phi_analyt(:,34),'r')
-    legend(type,'Analytical'), title([type,' at t = ',num2str(i)]), axis([0,length(phi)-1,-.5,1.5]);
+    %legend(type,'Analytical'), title([type,' at t = ',num2str(i)]), 
+    axis([0,length(phi)-1,-.5,1.5]); set(gca,'FontSize',14);
     hold off
     saveas(h,[type,'_',num2str(i),'.png']);
 end
